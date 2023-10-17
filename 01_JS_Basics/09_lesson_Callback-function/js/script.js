c956f3b92387bfa77e7eb77573b4d98ed7912916
Denis <KILLDEN@RAMBLER.RU>
2023-10-17 18:09:53 +0300

    Update 17.10.23

---
+++ 01_JS_Basics/09_lesson_Callback-function/js/script.js
@@ -0,0 +1,28 @@
+// ПЕРВОЕ ПРИЛОЖЕНИЕ!!!
+
+'use strict';
+
+// CALLBACK-FUNCTION : ЕСЛИ Ф-ЦИИ В КОДЕ ИДУТ ОДНА ЗА ДРУГОЙ, ТО ЭТО НЕ ЗНАЧИТ,
+// ЧТО ОНИ БУДУТ ВЫПОЛНЯТЬСЯ ТОЧНО В ТАКОЙ ЖЕ ПОСЛЕДОВАТЕЛЬНОСТИ!!!!
+
+function first() {
+  // do something и имеет задержку для общения с сервером
+  setTimeout(function() {
+    console.log(1);
+  }, 500);
+}
+
+function second() {
+  console.log(2);
+}
+first(); // обязательно необходимо вызывать функцию!!!! пишем в форате CamelCase!!!!
+second();
+
+function learnJS(lang, callback) { // классический пример коллбэка!!!!!!
+  console.log(`Я учу: ${lang}`);
+  callback(); // вызывем коллбэк
+}
+function done() {
+  console.log('Я прошел этот урок!');
+}
+learnJS('JavaScript', done);
---
+++ 01_JS_Basics/10_lesson_object(ES6)/js/script.js
@@ -0,0 +1,48 @@
+// ПЕРВОЕ ПРИЛОЖЕНИЕ!!!
+
+// 'use strict';
+
+const options = {
+  name: 'test',
+  width: 1024,
+  heigth: 1024,
+  colors: {
+    border: 'black',
+    bg: 'red',
+  },
+  makeTest: function() {
+    console.log('Test')
+  }
+};
+options.makeTest(); // мы можем создавать собственные методы!!!!!!!!!!!!!
+
+// ДЕСТРУКТУРИЗАЦИЯ ОБЪЕКТА!!!!
+const { border, bg } = options.colors;
+console.log(border);
+
+console.log(options.name);
+console.log(options.colors);
+console.log(options['colors']['border']);
+console.log(options.colors.border);
+console.log(options['colors']['bg']);
+console.log(options.colors.bg);
+// delete options.name; // оператор удаления
+// console.log(options);
+console.log(Object.keys(options)); // метод перебора ключей объекта
+// у строк и у массивово есть свойство length
+console.log(Object.keys(options).length); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// подсчет свойств во всем объекте!!!!
+let counter = 0;
+for (let key in options) {
+  if (typeof(options[key]) === 'object') {
+    for (let i in options[key]) {
+      console.log(`Свойство - ${i} имеет значение - ${options[key][i]}`);
+      // counter++; // всего свойств в объекте!!!
+    }
+  } else {
+    console.log(`Свойство - ${key} имеет значение - ${options[key]}`);
+    counter++; // количество свойств на верхнем уровне!!!!!
+  }
+}
+console.log(counter);
