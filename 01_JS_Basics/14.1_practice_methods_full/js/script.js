6203b6c2175250e8744e2ff4b5bd6771176ed278
Denis <KILLDEN@RAMBLER.RU>
2023-10-20 15:42:04 +0300

    Update 20.10.23

---
+++ 01_JS_Basics/14.1_practice_methods_full/js/script.js
@@ -0,0 +1,80 @@
+// ПЕРВОЕ ПРИЛОЖЕНИЕ!!!!
+
+// 1.задача переписать код, чтобы разрозненные функции стали методами объекта personalMovieDB:
+// 2.задача в методе writeYourGenres запретить пользователю нажимать кнопку "ОТМЕНА" или оставлять
+// пустую строку. В противном случяае отправлять его к этому же вопросу. После введения всех жанров,
+// с помощью метода forEach вывести в консоль сообщения в таком виде: "Любимый жанр №(номер по
+// порядку, начиная с 1-го) - это (название их массива)"*/
+const personalMovieDB = {
+  count: {},
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: {},
+  checkPrivatStatus: function() {
+    personalMovieDB.privat = confirm('Это частная информация?');
+    console.log(personalMovieDB.privat);
+  },
+  numbOfFilms: function() {
+    personalMovieDB.count = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+    while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+      personalMovieDB.count = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+    }
+  },
+  rememberMyFilms: function() {
+    for (let i = 0; i < 2; i++) {
+      const a = prompt('Один из последних просмотренных фильмов?', '');
+      const b = +prompt('На сколько оцените его?', '');  
+      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('DONE');
+      } else {
+        console.log('ERROR');
+        i--;
+      }
+    }
+  },
+  detectPersonalLevel: function() {
+    if (personalMovieDB.count <= 1) {
+      console.log('Вы не можете оценивать, результаты некорректны!');
+    } else if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
+      console.log('Просмотрено слишком мало фильмов для оценки!');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+      console.log('Вы классический зритель!');
+    } else if (personalMovieDB.count >= 30) {
+      console.log('Вы настоящий киноман!');
+    } else {
+      console.log('ERROR');
+    }
+  },
+  showMyDB: function() {
+    for (let i = 0; i < 1; i++) {
+      if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+      } else {
+        console.log('Это частная информация!');
+      }
+    }
+  },
+  writeYourGenres: function() {
+    for (let i = 1; i <= 3; i++) {
+      let genre = prompt(`Ваш любимый жанр под номером ${i}:`);
+      if (genre == '' || genre == null || !isNaN(genre)) {
+        console.log('Вы не ввели данные о жанре!');
+        i--;
+      } else {
+          personalMovieDB.genres[i - 1] = genre;
+      }
+    }
+    personalMovieDB.genres.forEach((item, i) => { // коллбэк ф-ия, где item - каждый перебираемый
+      // элемент массива - жанры, i - это номер по порядку в каждой итерации цикла
+      console.log(`Любимый жанр ${i + 1} - это ${item}`); // для сохранения номеров по порядку - прибавляем 1-ку!
+    });
+  },
+};
+personalMovieDB.checkPrivatStatus();
+personalMovieDB.numbOfFilms();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.showMyDB();
+personalMovieDB.writeYourGenres();
---
+++ 01_JS_Basics/15_lesson_dynamical_typification/js/script.js
@@ -0,0 +1,47 @@
+// // ПЕРВОЕ ПРИЛОЖЕНИЕ!!!
+
+// // 'use strict';
+
+// to String - все что получаем от пользователя, всегда будет иметь тип STRING.
+// Это и данные иp PROMPT, данные из INPUT / TEXTAREA и прочее. ВСЕ ЭТО СТРОКИ!!!
+// 1)
+console.log(typeof(String(null)));
+console.log(String(null));
+console.log(typeof(String(4)));
+
+// 2)
+console.log(typeof(5 + '')); // ПРИ СЛОЖЕНИИ СО СТРОКОЙ ПОЛУЧАЕМ СТРОКУ!!!
+
+const num = 5;
+console.log('http://vk.com/catalog' + num); // старый код, сейчас используется интерполяция,
+// ссылка на каталог для динамического перехода на пятую строчку каталога!!!
+
+const fontSize = 26 + 'px'; // динамическая типизация при передаче стилей в строковом выражении
+
+// to Number
+// 1)
+console.log(typeof(Number('4'))); // пользуются редко!
+
+// 2)
+console.log(typeof(+'5')); // применяется унарный плюс чаще!
+
+// 3)
+console.log(typeof(parseInt('15px', 10)));
+
+// to Boolean
+// 1)
+// 0, '', null, undefined, NaN - превращается в FALSE!!!!!!!!!!
+let switcher = null;
+if (switcher) {
+  console.log('Working...'); // тут скрипт работать не будет так как в switcher значение FALSE!
+}
+switcher = 1;
+if (switcher) {
+  console.log('Working...');// тут скрипт работать отработает так как в switcher значение TRUE!
+}
+
+// 2)
+console.log(typeof(Boolean('4'))); // булиновое значение
+
+// 3)
+console.log(typeof(!!'4444444')); // булиновое значение, крайне редко встречается!!!
