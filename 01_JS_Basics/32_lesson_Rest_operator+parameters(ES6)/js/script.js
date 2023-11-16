ad1c76a5f902b6875015b1fe593cfebe4b21e005
Denis <KILLDEN@RAMBLER.RU>
2023-11-16 10:49:38 +0300

    Update 16.11.23

---
+++ 01_JS_Basics/32_lesson_Rest_operator+parameters(ES6)/js/script.js
@@ -0,0 +1,21 @@
+// ПЕРВОЕ ПРИЛОЖЕНИЕ!!!
+
+'use strict';
+
+// Оператор REST записывается как(, ...rest)
+const log = function(a, b, ...rest) {
+	console.log(a, b, rest);
+};
+log('basic', 'else', 'operator', 'usage'); // получил - basic else [ 'operator', 'usage' ] - rest сущности оператор собрал в массив!!!
+
+//Параметры по умолчанию
+function calcOrDouble(number, basis) {
+	basis = basis || 2; // оператор ИЛИ вернет первую "правду", соответственно undefind || true и из этого выражения вернется 2, так как она имеет значение true
+	console.log(number * basis);
+}
+calcOrDouble(3); // но это не надежная проверка и в (ES6) параметры по умолчанию можно прописывать при объвлении функции =>
+
+function calcOrDouble2(number, basis = 2) {
+	console.log(number * basis);
+}
+calcOrDouble2(3);
