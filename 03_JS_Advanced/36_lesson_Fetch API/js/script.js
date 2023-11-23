5f41ca6ee526d36ec78c654d2511ea3ee65a8578
Denis <KILLDEN@RAMBLER.RU>
2023-11-23 17:14:42 +0300

    Update 23.11.23

---
+++ 03_JS_Advanced/36_lesson_Fetch API/js/script.js
@@ -0,0 +1,18 @@
+// ПЕРВОЕ ПРИЛОЖЕНИЕ!!!
+
+'use strict';
+
+//  https://jsonplaceholder.typicode.com/  - это виртуальная база данных в виде сервера для тестирования наших запросов на основе PROMISE!!!
+fetch('https://jsonplaceholder.typicode.com/posts', {
+	method: 'POST',
+	body: JSON.stringify({name: 'Alex'}),
+	headers: {
+		'Content-type': 'application/json'
+	}
+}) // 
+	.then(response => response.json())		// тут мы получаем ответ от сервера в формате json (JS объект, который можно дальше использовать), т.е. вернется PROMISE, если он успешен =>
+	.then(json => console.log(json));	// то дальше мы его используем в консоли
+
+// Получил:
+// {name: 'Alex', id: 101}id: 101name: "Alex"[[Prototype]]: Object
+// соответственно: нам вернулась 101 запись, так как на фетче уже 100 постов, это корректное поведение для проверки, с ним работа проще чем с http request
