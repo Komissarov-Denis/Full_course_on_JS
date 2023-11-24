e48c41b25aedede8f2a9457bbe616e533f15ddc8
Denis <KILLDEN@RAMBLER.RU>
2023-11-24 12:49:06 +0300

    Update 24.11.23

--- 02_JS_Practice/Food_project/src/js/script.js
+++ 02_JS_Practice/Food_project/src/js/script.js
@@ -189,10 +189,84 @@ window.addEventListener('DOMContentLoaded', () => {
 		// 'third__red',  // классы успешно добавляются
 	).render(); // заполняем новый класс MenuCards с помощью метода render()
 
-	// SEND-FORMS------------------------------------------------------------------------
+	// // SEND-FORMS----------------------------------------------------XMLHttpRequest()
+	// const forms = document.querySelectorAll('form');
+	// const message = {
+	// 	// loading: 'Загрузка...',
+	// 	loading: 'img/form/spinner.svg', // добавляем картинку спиннера вместо надписи в блоке div Загрузка...
+	// 	success: 'Спасибо! Скоро с Вами свяжемся!',
+	// 	failure: 'Что-то пошло не так...',
+	// };
+	// forms.forEach(item => { // берем все созданные формы и подвязываем функцию postData
+	// 	postData(item);
+	// });
+	// function postData(form) { // передавать будем какую-то форму, очень удобно навесить на нее обработчик события submit, которое будет срабатывать каждый раз при отправке форм
+	// 	form.addEventListener('submit', (e) => {
+	// 		e.preventDefault(); // отменяем дефолтную перезагрузку и поведение браузера
+	// 		// const statusMessage = document.createElement('div'); // создаем блок для сообщений
+	// 		const statusMessage = document.createElement('img'); // вместо блока теперь будем использовать картинку спиннера
+	// 		// statusMessage.classList.add('status'); // добавляем класс блоку сообщений
+	// 		statusMessage.src = message.loading; // используем путь к спиннеру
+	// 		statusMessage.textContent = message.loading; // заполняем блок главным сообщением 'Загрузка...'
+	// 		statusMessage.style.cssText = `
+	// 			display: block;
+	// 			margin: 0 auto;
+	// 		`; // добавляем стили спиннеру
+	// 		// form.append(statusMessage); // к форме добавляем это сообщение 'Загрузка...'
+	// 		form.insertAdjacentElement('afterend', statusMessage); // чтобы спиннер не сбивал верстку используем insertAdjacentElement()!!!
+	// 		const request = new XMLHttpRequest(); // создаем новый объект для формирования документа запроса
+	// 		request.open('POST', 'server.php');
+	// 		// request.setRequestHeader('Content-type', 'multipart/form-data'); // задаем заголовок контента для php...НО, В СВЯЗКЕ XMLHttpRequest() И FormData() - ЗАГОЛОВОК УСТАНАВЛИВАТЬ НЕ НУЖНО!!!
+	// 		request.setRequestHeader('Content-type', 'application/json'); // задаем заголовок контента для отправки в формате json, если этого затребует бэкэндер
+	// 		const formData = new FormData(form); // FormData(form) отыскивает в html атрибут name в тегах input всех форм, без него работать не будет!!!
+	// 		const objectJson = {}; // сождал новый объект для отправки данных в формате json
+	// 		formData.forEach(function(value, key) { // forEach переберет все, что есть внутри formData и заполнит objectJson
+	// 			objectJson[key] = value;
+	// 		});
+	// 		const json = JSON.stringify(objectJson); // конвертируем objectJson в строку JSON с двойными ковычками
+	// 		request.send(json); // отправляем запрос в формате json
+	// 		// request.send(formData); // отправляем вновь созданный объект formData КОММЕНТИРУЕМ/РАЗКОММЕНТИРУЕМ НУЖНЫЙ ФОРМАТ ОТПРАВКИ php/json
+	// 		request.addEventListener('load', () => {
+	// 			if (request.status === 200) {
+	// 				console.log(request.response);
+	// 				// statusMessage.textContent = message.success; // и так как statusMessage теперь стал DOM узлом на странице html, помещаем соощение 'Спасибо! Скоро с Вами свяжемся!'
+	// 				showThanksModal(message.success); // вместо statusMessage.textContent будет показываться модальное окно функции showThanksModal()!!!
+	// 				form.reset(); // очищаем форму после выведением сообщения
+	// 				// setTimeout(() => { // после делегирования событий функции showThanksModal убираем setTimeout(),
+	// 				statusMessage.remove(); //  так как statusMessage.remove() будет использоваться для loading спиннера, который будет отображаться на странице
+	// 				// }, 4000); // очистка формы через 4 секунды
+	// 			} else {
+	// 				// statusMessage.textContent = message.failure; // если произошел сбой, то помещаем 'Что-то пошло не так...'
+	// 				showThanksModal(message.failure);// вместо statusMessage.textContent будет показываться модальное окно функции showThanksModal()!!!
+	// 			}
+	// 		});
+	// 	}); 
+	// }
+	// function showThanksModal(message) { // создаем функцию динамической замены элементов мадального окна с отправкой сообщения message
+	// 	const prevModalDialog = document.querySelector('.modal__dialog'); // получаем элемент modal__dialog
+	// 	prevModalDialog.classList.add('hide'); // добавляем класс hide элементу modal__dialog
+	// 	openModalWindow(); // команда открытия модальных окон
+	// 	const thanksModal = document.createElement('div'); // создаем новый контент обертку
+	// 	thanksModal.classList.add('modal__dialog'); // будем заменять один modal__dialog другим с новым контентом
+	// 	thanksModal.innerHTML = ` 
+	// 		<div class="modal__content">
+	// 			<div class="modal__close" data-close>&times;</div>
+	// 			<div class="modal__title">${message}</div>
+	// 		</div>
+	// 	`; // создаем новый контент и в первоначальном скрипте (MODAL----) настраиваем ДЕЛЕГИРОВАНИЕ СОБЫТИЙ!!!
+	// 	document.querySelector('.modal').append(thanksModal); // помещаем новое модальное окно на страницу
+	// 	setTimeout(() => { // чтобы новый динамический блок исчезал через 4 сек. и появлялся предыдущий сверстанный блок modal__dialog, применим асинхронную операцию setTimeout()
+	// 		thanksModal.remove(); // thanksModal будем удалять, чтобы вновь созданные блоки не накапливались
+	// 		prevModalDialog.classList.add('show'); // заменяем классы отображения сверстанного модального окна modal__dialog
+	// 		prevModalDialog.classList.remove('hide');
+	// 		closeModalWindow(); // закрываем модальное окно, чтобы не мешать пользователю
+	// 	}, 4000);
+	// }
+
+	// SEND-FORMS--------------------------------------fetch() НОВЫЙ ТИП ЗАПРОСОВ гораздо ПРОЩЕ и КОРОЧЕ 
 	const forms = document.querySelectorAll('form');
 	const message = {
-		// loading: 'Загрузка...',
+		// loading: 'Загрузка...', // текст комментируем, так как будем использовать спиннер картинку
 		loading: 'img/form/spinner.svg', // добавляем картинку спиннера вместо надписи в блоке div Загрузка...
 		success: 'Спасибо! Скоро с Вами свяжемся!',
 		failure: 'Что-то пошло не так...',
@@ -213,33 +287,29 @@ window.addEventListener('DOMContentLoaded', () => {
 				margin: 0 auto;
 			`; // добавляем стили спиннеру
 			// form.append(statusMessage); // к форме добавляем это сообщение 'Загрузка...'
-			form.insertAdjacentElement('afterend', statusMessage); // чтобы спиннер не сбивал верстку используем insertAdjacentElement()!!!
-			const request = new XMLHttpRequest(); // создаем новый объект для формирования документа запроса
-			request.open('POST', 'server.php');
-			// request.setRequestHeader('Content-type', 'multipart/form-data'); // задаем заголовок контента для php...НО, В СВЯЗКЕ XMLHttpRequest() И FormData() - ЗАГОЛОВОК УСТАНАВЛИВАТЬ НЕ НУЖНО!!!
-			request.setRequestHeader('Content-type', 'application/json'); // задаем заголовок контента для отправки в формате json, если этого затребует бэкэндер
-			const formData = new FormData(form); // FormData(form) отыскивает в html атрибут name в тегах input всех форм, без него работать не будет!!!
+			form.insertAdjacentElement('afterend', statusMessage); // чтобы спиннер не сбивал верстку используем insertAdjacentElement() - вставить соседний элемент ()!!!
+			const formData = new FormData(form); // FormData(form) отыскивает в html атрибут name в тегах input всех форм, без него работать не будет!!!			
 			const objectJson = {}; // сождал новый объект для отправки данных в формате json
 			formData.forEach(function(value, key) { // forEach переберет все, что есть внутри formData и заполнит objectJson
 				objectJson[key] = value;
 			});
-			const json = JSON.stringify(objectJson); // конвертируем objectJson в строку JSON с двойными ковычками
-			request.send(json); // отправляем запрос в формате json
-			// request.send(formData); // отправляем вновь созданный объект formData КОММЕНТИРУЕМ/РАЗКОММЕНТИРУЕМ НУЖНЫЙ ФОРМАТ ОТПРАВКИ php/json
-			request.addEventListener('load', () => {
-				if (request.status === 200) {
-					console.log(request.response);
-					// statusMessage.textContent = message.success; // и так как statusMessage теперь стал DOM узлом на странице html, помещаем соощение 'Спасибо! Скоро с Вами свяжемся!'
+			fetch('server.php', { // создаем новый объект для формирования документа запроса fetch(), метод и заголовки указывать обязательно!!!
+				method: 'POST',
+				headers: {
+					'Content-type': 'application/json' // закомментировал, так как пока не JSON отправляем!!!
+				},
+				body: JSON.stringify(objectJson), // конвертируем objectJson в строку JSON с двойными ковычками
+			})	
+				.then(data => data.text())
+				.then(data => { // сервер вернет данные data, пока это не JSON
+					console.log(data); // берем data данные, которые вернул сервер из PROMISE (успешный исход)
 					showThanksModal(message.success); // вместо statusMessage.textContent будет показываться модальное окно функции showThanksModal()!!!
-					form.reset(); // очищаем форму после выведением сообщения
-					// setTimeout(() => { // после делегирования событий функции showThanksModal убираем setTimeout(),
-					statusMessage.remove(); //  так как statusMessage.remove() будет использоваться для loading спиннера, который будет отображаться на странице
-					// }, 4000); // очистка формы через 4 секунды
-				} else {
-					// statusMessage.textContent = message.failure; // если произошел сбой, то помещаем 'Что-то пошло не так...'
-					showThanksModal(message.failure);// вместо statusMessage.textContent будет показываться модальное окно функции showThanksModal()!!!
-				}
-			});
+					statusMessage.remove(); // удаляем наш спинер по выполнению PROMISE
+				}).catch(() => { // catch метод обязательно нужно прописывать для обратоток ошибок!!!
+					showThanksModal(message.failure);// вместо statusMessage.textContent будет показываться модальное окно функции showThanksModal()!!!				
+				}).finally(() => { // finally метод обязательно нужно прописывать для обратоток оконечных действий
+					form.reset(); // очищаем форму после выведением сообщения				
+				});
 		}); 
 	}
 	function showThanksModal(message) { // создаем функцию динамической замены элементов мадального окна с отправкой сообщения message
--- 03_JS_Advanced/36_lesson_Fetch API/js/script.js
+++ 03_JS_Advanced/36_lesson_Fetch API/js/script.js
@@ -17,4 +17,73 @@ fetch('https://jsonplaceholder.typicode.com/posts', {
 // {name: 'Alex', id: 101}id: 101name: "Alex"[[Prototype]]: Object
 // соответственно: нам вернулась 101 запись, так как на фетче уже 100 постов, это корректное поведение для проверки, с ним работа проще чем с XMLHttpRequest
 
-// fetch() запрос для проекта FOOD:
+// ОБРАЗЕЦ fetch() запроса для проекта FOOD:
+// SEND-FORMS--------------------------------------fetch() НОВЫЙ ТИП ЗАПРОСОВ гораздо ПРОЩЕ и КОРОЧЕ 
+const forms = document.querySelectorAll('form');
+const message = {
+	// loading: 'Загрузка...', // текст комментируем, так как будем использовать спиннер картинку
+	loading: 'img/form/spinner.svg', // добавляем картинку спиннера вместо надписи в блоке div Загрузка...
+	success: 'Спасибо! Скоро с Вами свяжемся!',
+	failure: 'Что-то пошло не так...',
+};
+forms.forEach(item => { // берем все созданные формы и подвязываем функцию postData
+	postData(item);
+});
+function postData(form) { // передавать будем какую-то форму, очень удобно навесить на нее обработчик события submit, которое будет срабатывать каждый раз при отправке форм
+	form.addEventListener('submit', (e) => {
+		e.preventDefault(); // отменяем дефолтную перезагрузку и поведение браузера
+		// const statusMessage = document.createElement('div'); // создаем блок для сообщений
+		const statusMessage = document.createElement('img'); // вместо блока теперь будем использовать картинку спиннера
+		// statusMessage.classList.add('status'); // добавляем класс блоку сообщений
+		statusMessage.src = message.loading; // используем путь к спиннеру
+		statusMessage.textContent = message.loading; // заполняем блок главным сообщением 'Загрузка...'
+		statusMessage.style.cssText = `
+			display: block;
+			margin: 0 auto;
+		`; // добавляем стили спиннеру
+		// form.append(statusMessage); // к форме добавляем это сообщение 'Загрузка...'
+		form.insertAdjacentElement('afterend', statusMessage); // чтобы спиннер не сбивал верстку используем insertAdjacentElement() - вставить соседний элемент ()!!!
+		const formData = new FormData(form); // FormData(form) отыскивает в html атрибут name в тегах input всех форм, без него работать не будет!!!			
+		const objectJson = {}; // сождал новый объект для отправки данных в формате json
+		formData.forEach(function(value, key) { // forEach переберет все, что есть внутри formData и заполнит objectJson
+			objectJson[key] = value;
+		});
+		fetch('server.php', { // создаем новый объект для формирования документа запроса fetch(), метод и заголовки указывать обязательно!!!
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json' // закомментировал, так как пока не JSON отправляем!!!
+			},
+			body: JSON.stringify(objectJson), // конвертируем objectJson в строку JSON с двойными ковычками
+		})	
+			.then(data => data.text())
+			.then(data => { // сервер вернет данные data, пока это не JSON
+				console.log(data); // берем data данные, которые вернул сервер из PROMISE (успешный исход)
+				showThanksModal(message.success); // вместо statusMessage.textContent будет показываться модальное окно функции showThanksModal()!!!
+				statusMessage.remove(); // удаляем наш спинер по выполнению PROMISE
+			}).catch(() => { // catch метод обязательно нужно прописывать для обратоток ошибок!!!
+				showThanksModal(message.failure);// вместо statusMessage.textContent будет показываться модальное окно функции showThanksModal()!!!				
+			}).finally(() => { // finally метод обязательно нужно прописывать для обратоток оконечных действий
+				form.reset(); // очищаем форму после выведением сообщения				
+			});
+	}); 
+}
+function showThanksModal(message) { // создаем функцию динамической замены элементов мадального окна с отправкой сообщения message
+	const prevModalDialog = document.querySelector('.modal__dialog'); // получаем элемент modal__dialog
+	prevModalDialog.classList.add('hide'); // добавляем класс hide элементу modal__dialog
+	openModalWindow(); // команда открытия модальных окон
+	const thanksModal = document.createElement('div'); // создаем новый контент обертку
+	thanksModal.classList.add('modal__dialog'); // будем заменять один modal__dialog другим с новым контентом
+	thanksModal.innerHTML = ` 
+		<div class="modal__content">
+			<div class="modal__close" data-close>&times;</div>
+			<div class="modal__title">${message}</div>
+		</div>
+	`; // создаем новый контент и в первоначальном скрипте (MODAL----) настраиваем ДЕЛЕГИРОВАНИЕ СОБЫТИЙ!!!
+	document.querySelector('.modal').append(thanksModal); // помещаем новое модальное окно на страницу
+	setTimeout(() => { // чтобы новый динамический блок исчезал через 4 сек. и появлялся предыдущий сверстанный блок modal__dialog, применим асинхронную операцию setTimeout()
+		thanksModal.remove(); // thanksModal будем удалять, чтобы вновь созданные блоки не накапливались
+		prevModalDialog.classList.add('show'); // заменяем классы отображения сверстанного модального окна modal__dialog
+		prevModalDialog.classList.remove('hide');
+		closeModalWindow(); // закрываем модальное окно, чтобы не мешать пользователю
+	}, 4000);
+}
