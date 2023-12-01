0d6c38e70fba7ac524236bae78ec93e7f019dfb1
Denis <KILLDEN@RAMBLER.RU>
2023-12-01 17:26:54 +0300

    Update 01.12.23

--- 02_JS_Practice/Food_project/dist/js/script.js
+++ 02_JS_Practice/Food_project/dist/js/script.js
@@ -163,7 +163,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 		return await result.json(); // возвращаем из функции postData промис (result.json()) для дальнейшей обработки через чепочку .then() - но это АСИНХРОННЫЙ КОД + await дожидается обработки данных в result.json()!!!
 	};
-	// getResources('http://localhost:3000/menu')
+	// getResources('http://localhost:3000/menu') => еще вариант формирования MenuCards
 	// 	.then(data => createMenuCards(data));
 	// function createMenuCards(data) {
 	// 	data.forEach(({img, altimg, title, descr, price}) => {
@@ -188,7 +188,7 @@ window.addEventListener('DOMContentLoaded', () => {
 				new MenuCards(img, altimg, title, descr, price, '.menu .container').render(); // запускаем конструктор - MenuCards() для заполнения - render() карточек меню столько раз, сколько объектов в массиве db.json
 			});
 		});
-	// new MenuCards(
+	// new MenuCards(    => заменили верстку динамическим формированием MenuCards с помощью запросов к серверу
 	// 	'img/tabs/vegy.jpg',
 	// 	'vegy',
 	// 	'Меню "Фитнес"',
@@ -300,8 +300,43 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 	
 	// SLIDER-------------------------------------ПРОСТОЙ ВАРИАНТ--------------------
-
-
+	const slides = document.querySelectorAll('.offer__slide'); // получаем все слайды на странице
+	const prev = document.querySelector('.offer__slider-prev'); // получаем стрелки перелистывания слайдов
+	const next = document.querySelector('.offer__slider-next'); // получаем стрелки перелистывания слайдов
+	const totalSlides = document.querySelector('#total'); // получаем значение элементов по идентификатору
+	const currentSlide = document.querySelector('#current'); // получаем значение элемента по идентификатору
+	let slideIndex = 1; // назначаем индекс каждому слайду
+	showSlides(slideIndex); // инициализируем функцию showSlides() со значением "1"
+	if (slides.length < 10) { // если количество слайдов меньше
+		totalSlides.textContent = `0${slides.length}`; // то добавляем к порядковому значению слайда "0"
+	} else { // иначе
+		totalSlides.textContent = slides.length; // просто записываем порядковое значение слайда
+	}
+	function showSlides(n)  { // присваиваем порядковый номер каждому слайду "n"
+		if (n > slides.length) {// если количество слайдов slides.length меньше порядкового номера слайда "n"
+			slideIndex = 1; // если ушли в "правую границу" слайдов, то перемещаемся в самое "начало" => slideIndex = 1
+		}
+		if (n < 1) {// если порядковый номер слайда "n" меньше 1
+			slideIndex = slides.length; // если ушли в "левую границу" слайдов, то перемещаемся в самый "конец" => slideIndex = slides.length
+		}
+		slides.forEach(item => item.style.display = 'none'); // сначала скрываем все слайды на страничке
+		slides[slideIndex - 1].style.display = 'block' ; // потом по нажатию показываем нужный слайд, выбираем [slideIndex - 1] так как массив начинается с "0"
+		if (slides.length < 10) { // если количество слайдов меньше
+			currentSlide.textContent = `0${slideIndex}`; // то добавляем к порядковому значению слайда "0"
+		} else { // иначе
+			currentSlide.textContent = slideIndex; // просто записываем порядковое значение слайда
+		}
+	}
+	function plusSlide(n) { // перебираем слайды по нажатию на стрелочки
+		showSlides(slideIndex += n); // если n=1, то прибавляем, если n=-1, то отнимаем 
+	}
+	prev.addEventListener('click', () => { // при нажатии на стрелочку "влево", передаем в функцию plusSlide() минус один
+		plusSlide(-1);
+	});
+	next.addEventListener('click', () => { // при нажатии на стрелочку "вправо", передаем в функцию plusSlide() плюс один
+		plusSlide(1);
+	});
 	
-	// SLIDER-------------------------------------БОЛЕЕ СЛОЖНЫЙ ВАРИАНТ--------------
+	// CAROUSEL-------------------------------------БОЛЕЕ СЛОЖНЫЙ ВАРИАНТ------------
+	
 });
