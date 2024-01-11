18cfddc9ff0e2ad4585fef11e12d00cb64c4cac1
Denis <KILLDEN@RAMBLER.RU>
2024-01-11 18:52:07 +0300

    Update 11.01.24

--- 02_JS_Practice/Food_project/dist/webpack/script.bundle.js
+++ 02_JS_Practice/Food_project/dist/webpack/script.bundle.js
@@ -1,3 +1,26 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+/*!**************************!*\
+  !*** ./src/js/script.js ***!
+  \**************************/
+__webpack_require__.r(__webpack_exports__);
 /* eslint-disable linebreak-style */
 window.addEventListener('DOMContentLoaded', () => {
 
@@ -569,3 +592,6 @@ window.addEventListener('DOMContentLoaded', () => {
 	getDynamicInformation('#age');
 
 });
+/******/ })()
+;
+//# sourceMappingURL=script.bundle.js.map
