fc9a554e61ff85081179504de33f8afa0d30f704
Denis <KILLDEN@RAMBLER.RU>
2024-02-15 11:35:36 +0300

    Update 15.02.24

--- 01_JS_Basics/01_lesson_data/js/script.js
+++ 01_JS_Basics/01_lesson_data/js/script.js
@@ -37,7 +37,65 @@ console.log(someThing);  // получил: someThing is not defined
 let und;
 console.log(und); // получил: undefined - значение не определено
 
-//-----------------------------------------------------------------
+// Виды запросов на основе модальных окон браузера: alert(), confirm(), prompt()
+alert('Hallo!');
+
+const result = confirm('Are you here?');
+console.log(result);
+
+const answer = prompt('Вам есть 18?', 'Да, мне 18!');
+console.log(typeof (answer)); // ВСЯ ИНФА ОТ ПОЛЬЗОВАТЕЛЯ - ПРИХОДИТ В ВИДЕ СТРОК!!!
+
+const answer2 = +prompt('Вам есть 18?', 'Да, мне 18!');
+console.log(answer2 + 5);  // +prompt, Т.Е "+" ПЕРЕВОДИТ СТРОКОВЫЕ В ЧИСЛОВЫЕ ДАНННЫЕ!!!
+
+const answers = [];
+answers[0] = prompt('Как Ваше имя?', '');
+answers[1] = prompt('Как Ваша фамилия?', '');
+answers[2] = prompt('Сколько Вам лет?', '');
+document.write(answers);
+console.log(typeof (answers));
+
+const category = 'toys';
+console.log('http://someurl.com/' + category + '/' + '5'); // пример конкотинации или объединения строк!!! получил: http://someurl.com/toys/5
+console.log(`http://someurl.com/${category}/5`); // `бэктики` или косые ковычки для интерполяции!!!Более УДОБНО!!! получил: http://someurl.com/toys/5
+
+const user = 'DEN';
+alert(`Привет, ${user}`);
+
+console.log('arr' + ' - object'); // получил: arr - object
+console.log(4 + ' - object'); // получил: 4 - object
+console.log(4 + +'5'); // унарный плюс без пробела!!! получил: 9
+
+let incr = 10;
+let decr = 10;
+incr++; // оператор инкремента, увеличения на 1 - постфиксный способ записи
+decr--; // оператор декремента, уменшения на 1 - постфиксный способ записи
+++incr; // оператор инкремента, увеличения на 1 - префиксный способ записи
+--decr; // оператор декремента, уменшения на 1 - префиксный способ записи
+console.log(++incr); // получил: 13
+console.log(--decr); // получил: 7
+
+console.log(5%2); // получил: 1, остаток от деления двух чисел, получил: 1 !!!
+
+console.log(2 * 4 == 8); // сравнение, получил: true
+console.log(2 * 4 == '8'); // сравнение, получил: true
+console.log(2 * 4 === 7); // строгое равенство, получил: false
+console.log(2 * 4 === '8'); // строгое равенство, получил: false
+
+// && - оператор И
+// || - оператор ИЛИ
+const isCheked = true;
+const isClosed = false;
+console.log(isCheked && isClosed); // получил: false
+console.log(isCheked || isClosed); // получил: true
+console.log(isCheked && !isClosed); // получил: true, ! - заменяет на обратное значение аргумента
+
+console.log((2 + 2) * 2 === 8); // строгое равенство, получил: true
+console.log(2 + 2 * 2 != 8); // не равенство, получил: true
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#Table - таблица приоритета операторов
+
+//---------------------ОБЪЕКТЫ-И-МАССИВЫ----------------------------
 
 const obj1 = { // Объект - это коллекция хранения данных: ключ и его значение, а также действия - это методы
 	name: 'John',
@@ -75,12 +133,19 @@ const arrObj2 = {
 console.log(arr3[2]); // получил: c
 console.log(arrObj2[2]); // получил: c
 arrObj2.v = '1234';
-console.log(arrObj2['v']); // получил: 1234
+console.log(arrObj2['v']); // получил: 1234 - скобки и через точку равнозначны, но через скобки безопаснее и надежнее!!!
 console.log(arrObj2.v); // получил: 1234
-
+const k = 'k';
+arrObj2.k = '5555';
+console.log(arrObj2[k]); // получил: 5555 - заранее объявляя переменную со строковым значением 'k' получаем тот же результат!!!
 
 const obj3 = {a:1, b:2, c:3}; // объекты - это структура хранения данных в парном формате: ключ и значение
 console.log(obj3.b); // получил: 2
+//У МАССИВОВ ПОДОБНЫЕ МАНИПУЛЯЦИИ НЕ РЕКОМЕНДОВАНЫ!!!
+const arr4 = ['a', 'b', 'c'];
+arr4[10] = 'y';
+console.log(arr4[10]); // был создан 11 элемент массива 'y'
+console.log(arr4); // получил: [ 'a', 'b', 'c', <7 empty items>, 'y' ] - появились пустые элементы в массиве, так как это нарушает логику массива
 
 const salary = {
 	Ann: 500,
@@ -88,62 +153,16 @@ const salary = {
 }; // оба варианта записи ключей подходят, но без кавычек быстрее
 console.log(salary.Alice); // получил: 800
 
+const arrObj3 = {
+	a: 'a',
+	'1': 'b',
+	2: 'c',
+	abc: { // допустимо внутри объекта записывать еще объект, а также массив
+		def: {
+			dd: [1, 2, 3, {}],
+			y: '10',
+		}
+	}
+};
+console.log(arrObj3); // получил: { '1': 'b', '2': 'c', a: 'a', abc: { def: { dd: [Array], y: '10' } } }
 //------------------------------------------------------------------
-
-alert('Hallo!');
-
-const result = confirm('Are you here?');
-console.log(result);
-
-const answer = prompt('Вам есть 18?', 'Да, мне 18!');
-console.log(typeof (answer)); // ВСЯ ИНФА ОТ ПОЛЬЗОВАТЕЛЯ - ПРИХОДИТ В ВИДЕ СТРОК!!!
-
-const answer2 = +prompt('Вам есть 18?', 'Да, мне 18!');
-console.log(answer2 + 5);  // +prompt, Т.Е "+" ПЕРЕВОДИТ СТРОКОВЫЕ В ЧИСЛОВЫЕ ДАНННЫЕ!!!
-
-const answers = [];
-answers[0] = prompt('Как Ваше имя?', '');
-answers[1] = prompt('Как Ваша фамилия?', '');
-answers[2] = prompt('Сколько Вам лет?', '');
-
-document.write(answers);
-console.log(typeof (answers));
-
-const category = 'toys';
-console.log('http://someurl.com/' + category + '/' + '5'); // пример конкотинации или объединения строк!!!
-console.log(`http://someurl.com/${category}/5`); // `бэктики` или косые ковычки для интерполяции!!!Более УДОБНО!!!
-
-const user = 'DEN';
-alert(`Привет, ${user}`);
-
-console.log('arr' + ' - object');
-console.log(4 + ' - object');
-console.log(4 + +'5'); // унарный плюс без пробела!!!!
-
-let incr = 10;
-let decr = 10;
-incr++; // оператор инкремента, увеличения на 1 - постфиксный способ записи
-decr--; // оператор декремента, уменшения на 1 - постфиксный способ записи
-++incr; // оператор инкремента, увеличения на 1 - префиксный способ записи
---decr; // оператор декремента, уменшения на 1 - префиксный способ записи
-console.log(++incr);
-console.log(--decr);
-
-console.log(5%2); // получил: 1, остаток от деления двух чисел = 1 !!!
-
-console.log(2 * 4 == 8); // сравнение
-console.log(2 * 4 == '8'); // сравнение
-console.log(2 * 4 === 7); // строгое равенство
-console.log(2 * 4 === '8'); // строгое равенство
-
-// && - оператор И
-// || - оператор ИЛИ
-const isCheked = true;
-const isClosed = false;
-console.log(isCheked && isClosed);
-console.log(isCheked || isClosed);
-console.log(isCheked && !isClosed); // ! - заменяет на обратное значение аргумента
-
-console.log((2 + 2) * 2 === 8); // строгое равенство
-console.log(2 + 2 * 2 != 8); // не равенство
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#Table - таблица приоритета операторов
