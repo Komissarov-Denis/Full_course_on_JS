05e6e5e2624c8b9296231cc621fc99357a731858
Denis <KILLDEN@RAMBLER.RU>
2024-02-16 14:46:58 +0300

    Update 16.02.24

--- 01_JS_Basics/14.1_practice_methods_full/js/script.js
+++ 01_JS_Basics/14.1_practice_methods_full/js/script.js
@@ -4,7 +4,7 @@
 
 // 1.задача переписать код, чтобы разрозненные функции стали методами объекта personalMovieDB:
 // 2.задача в методе writeYourGenres запретить пользователю нажимать кнопку "ОТМЕНА" или оставлять
-// пустую строку. В противном случяае отправлять его к этому же вопросу. После введения всех жанров,
+// пустую строку. В противном случае, отправлять его к этому же вопросу. После введения всех жанров,
 // с помощью метода forEach вывести в консоль сообщения в таком виде: "Любимый жанр №(номер по
 // порядку, начиная с 1-го) - это (название их массива)"*/
 const personalMovieDB = {
@@ -16,24 +16,42 @@ const personalMovieDB = {
 	checkPrivatStatus: function() {
 		personalMovieDB.privat = confirm('Это частная информация?');
 		console.log(personalMovieDB.privat);
+		personalMovieDB.showMyDB();
+	},
+	showMyDB: function() {
+		if (personalMovieDB.privat == false) {
+			console.log(personalMovieDB);
+			console.log('Открытый доступ!');
+			alert('Открытый доступ!');
+		} else {
+			console.log('Это частная информация!');
+			alert('Это частная информация!');
+		}
 	},
 	numbOfFilms: function() {
 		personalMovieDB.count = +prompt('Сколько фильмов Вы уже посмотрели?', '');
-		while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count) || personalMovieDB.count <= 1) {
+		while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count) || personalMovieDB.count <= 0) {
 			alert('Вы не можете оценивать, результаты будут некорректны!');
 			personalMovieDB.count = +prompt('Сколько фильмов Вы уже посмотрели?', '');
 		}
 	},
 	detectPersonalLevel: function() {
-		if (personalMovieDB.count > 1 && personalMovieDB.count <= 10) {
-			console.log('Просмотрено слишком мало фильмов для оценки!');
-		} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+		if (personalMovieDB.count >= 1 && personalMovieDB.count <= 3) {
+			console.log('Просмотрено слишком мало фильмов для оценки! Опрос завершен!');			
+			alert('Просмотрено слишком мало фильмов для оценки! Опрос завершен!');
+			return; 
+		}	else if (personalMovieDB.count > 3 && personalMovieDB.count < 30) {
 			console.log('Вы классический зритель!');
-		} else if (personalMovieDB.count >= 30) {
+			alert('Вы классический зритель!');
+		}	else if (personalMovieDB.count >= 30) {
 			console.log('Вы настоящий киноман!');
-		} else {
+			alert('Вы настоящий киноман!');
+		}	else {
 			console.log('ERROR');
+			alert('ERROR');
 		}
+		personalMovieDB.rememberMyFilms();
+		personalMovieDB.writeYourGenres();
 	},
 	rememberMyFilms: function() {
 		const num = personalMovieDB.count;
@@ -49,17 +67,6 @@ const personalMovieDB = {
 			} else {
 				console.log('ERROR');
 				i--;
-			}
-		}
-	},
-	showMyDB: function() {
-		for (let i = 0; i < 1; i++) {
-			if (personalMovieDB.privat == false) {
-				console.log(personalMovieDB);
-				console.log('Открытый доступ!');
-			} else {
-				console.log('Это частная информация!');
-				continue;
 			}
 		}
 	},
@@ -79,11 +86,8 @@ const personalMovieDB = {
 	},
 };
 personalMovieDB.checkPrivatStatus();
-personalMovieDB.showMyDB();
 personalMovieDB.numbOfFilms();
 personalMovieDB.detectPersonalLevel();
-personalMovieDB.rememberMyFilms();
-personalMovieDB.writeYourGenres();
 
 
 // const result = document.querySelector('.calculating__result span'); // получили по селектору класс, в который будем записывать результат расчета
