2846b1b6d834265656425863ade51eac1e2c171b
Denis <KILLDEN@RAMBLER.RU>
2024-02-22 12:48:27 +0300

    Update 22.02.24

--- 01_JS_Basics/10_lesson_objects(ES6)/js/script.js
+++ 01_JS_Basics/10_lesson_objects(ES6)/js/script.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-// ОБЪЕКТЫ в JS - это ассоциативные массивы!!!
+// ОБЪЕКТЫ в JS - это ассоциативные массивы, кототые могу хранить в себе любые типы данных в формате КЛЮЧ: ЗНАЧЕНИЕ, могут быть и вложенные!!!
 // const obj = new Object {}; - такой способ не используется...
 
 const options = {
@@ -13,7 +13,7 @@ const options = {
 		border: 'black',
 		bg: 'red',
 	},
-	makeTest: function() {
+	makeTest: function() { // метод который работает в объекте!!!
 		console.log('Test'); // получил: Test, так записываются методы!!!
 	}
 };
@@ -30,18 +30,7 @@ console.log(options); // получил:
 // 	makeTest: [Function: makeTest]
 // }
 
-// ДЕСТРУКТУРИЗАЦИЯ ОБЪЕКТА!!!!
-const {border, bg} = options.colors; // запись деструктурированного объекта!!!
-console.log(border); // получил: black, вытаскиваем кусочки свойств в качестве отдельных переменных!!!
-console.log(bg); // получил: red, вытаскиваем кусочки свойств в качестве отдельных переменных!!!
-console.log(options.name); // получил: test
-console.log(options.colors); // получил: { border: 'black', bg: 'red' }
-console.log(options['colors']['border']);  // получил: black, запись не особо правильная, для этого существует деструктуризация объекта =>
-console.log(options.colors.border);  // получил: black
-console.log(options['colors']['bg']); // получил: red
-console.log(options.colors.bg); // получил: red
-
-// подсчет свойств во всем объекте for (in) !!!
+// ПОДСЧЕТ СВОЙСТВ во всем объекте for (in) !!!
 let counter = 0; // переменная счетчик, подсчитывающая количество ключевых свойств объекта
 for (let key in options) { // перебираем все ключевые свойства внутри объекта options
 	if (typeof(options[key]) === 'object') { // если ключ является объектом, то запускаем перебор внутри перебора!!!
@@ -64,5 +53,18 @@ console.log(counter); // получил:
 // }
 // 3
 
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 console.log(Object.keys(options));  // получил: [ 'width', 'heigth', 'colors', 'makeTest' ], метод перебора ключей объекта после удаления ключа: name!!!
 console.log(Object.keys(options).length);  // получил: 4, у строк и у массивов есть свойство length!!!
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// ДЕСТРУКТУРИЗАЦИЯ ОБЪЕКТА ES6!!!
+const {border, bg} = options.colors; // запись ДЕСТРУКТУРИРОВАННОГО объекта!!!
+console.log(border); // получил: black, вытаскиваем кусочки свойств в качестве отдельных переменных из деструктурированного объекта!!!
+console.log(bg); // получил: red, вытаскиваем кусочки свойств в качестве отдельных переменных из деструктурированного объекта!!!
+console.log(options.name); // получил: test
+console.log(options.colors); // получил: { border: 'black', bg: 'red' }
+console.log(options['colors']['border']);  // получил: black, запись не особо правильная, для этого существует деструктуризация объекта =>
+console.log(options.colors.border);  // получил: black
+console.log(options['colors']['bg']); // получил: red
+console.log(options.colors.bg); // получил: red
