f093ba11a91deed8cc2a265484bbd482ae1b87f1
Denis <KILLDEN@RAMBLER.RU>
2024-02-22 12:39:16 +0300

    Update 22.02.24

--- 01_JS_Basics/10_lesson_objects(ES6)/js/script.js
+++ 01_JS_Basics/10_lesson_objects(ES6)/js/script.js
@@ -1,11 +1,14 @@
 // ПЕРВОЕ ПРИЛОЖЕНИЕ!!!
 
-// 'use strict';
+'use strict';
+
+// ОБЪЕКТЫ в JS - это ассоциативные массивы!!!
+// const obj = new Object {}; - такой способ не используется...
 
 const options = {
 	name: 'test',
 	width: 1024,
-	heigth: 1024,
+	heigth: 720,
 	colors: {
 		border: 'black',
 		bg: 'red',
@@ -14,7 +17,18 @@ const options = {
 		console.log('Test'); // получил: Test, так записываются методы!!!
 	}
 };
-options.makeTest(); // мы можем создавать собственные методы!!!
+options.makeTest(); // мы можем создавать собственные методы в объектах!!!
+console.log(options.width); // получил: 1024
+console.log(options['heigth']); // получил: 720
+
+delete options.name;// оператор удаления
+console.log(options); // получил: 
+// {
+// 	width: 1024,
+// 	heigth: 720,
+// 	colors: { border: 'black', bg: 'red' },
+// 	makeTest: [Function: makeTest]
+// }
 
 // ДЕСТРУКТУРИЗАЦИЯ ОБЪЕКТА!!!!
 const {border, bg} = options.colors; // запись деструктурированного объекта!!!
@@ -27,29 +41,17 @@ console.log(options.colors.border);  // получил: black
 console.log(options['colors']['bg']); // получил: red
 console.log(options.colors.bg); // получил: red
 
-delete options.name;// оператор удаления
-console.log(options); // получил: 
-// {
-// 	width: 1024,
-// 	heigth: 1024,
-// 	colors: { border: 'black', bg: 'red' },
-// 	makeTest: [Function: makeTest]
-// }
-
-console.log(Object.keys(options));  // получил: [ 'width', 'heigth', 'colors', 'makeTest' ], метод перебора ключей объекта после удаления ключа: name!!!
-console.log(Object.keys(options).length);  // получил: 4, у строк и у массивов есть свойство length!!!
-
-// подсчет свойств во всем объекте!!!!
-let counter = 0;
-for (let key in options) {
-	if (typeof(options[key]) === 'object') {
-		for (let i in options[key]) {
-			console.log(`Свойство - ${i} имеет значение - ${options[key][i]}`);
-			// counter++; // всего свойств в объекте!!!
+// подсчет свойств во всем объекте for (in) !!!
+let counter = 0; // переменная счетчик, подсчитывающая количество ключевых свойств объекта
+for (let key in options) { // перебираем все ключевые свойства внутри объекта options
+	if (typeof(options[key]) === 'object') { // если ключ является объектом, то запускаем перебор внутри перебора!!!
+		for (let i in options[key]) { // теперь перебираем i - это ключи внутри встроенного объекта colors
+			console.log(`Свойство - ${i} имеет значение - ${options[key][i]}`); // ${options[key][i]} так мы достучимся до значений ключей border и bg
+			// counter++; // всего свойств в объекте!!! получил 5
 		}
 	} else {
-		console.log(`Свойство - ${key} имеет значение - ${options[key]}`);
-		counter++; // количество свойств на верхнем уровне!!!!!
+		console.log(`Свойство - ${key} имеет значение - ${options[key]}`); // если ключ не является объектом, то выводим значения ключей options{}
+		counter++; // количество свойств на верхнем уровне!!!!! Но этот метод не удобен!!! есть Object.keys()!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	}
 }
 console.log(counter); // получил:
@@ -61,3 +63,6 @@ console.log(counter); // получил:
 // 		console.log('Test'); // получил: Test, так записываются методы!!!
 // }
 // 3
+
+console.log(Object.keys(options));  // получил: [ 'width', 'heigth', 'colors', 'makeTest' ], метод перебора ключей объекта после удаления ключа: name!!!
+console.log(Object.keys(options).length);  // получил: 4, у строк и у массивов есть свойство length!!!
