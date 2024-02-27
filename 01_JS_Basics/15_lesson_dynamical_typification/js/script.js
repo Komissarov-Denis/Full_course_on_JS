d0990a7879f0987e344e3a3c31134182fce71860
Denis <KILLDEN@RAMBLER.RU>
2024-02-27 15:24:46 +0300

    Update 27.02.24

--- 01_JS_Basics/15_lesson_dynamical_typification/js/script.js
+++ 01_JS_Basics/15_lesson_dynamical_typification/js/script.js
@@ -2,47 +2,49 @@
 
 'use strict';
 
-// to String - все что получаем от пользователя, всегда будет иметь тип STRING. Это и данные от PROMPT, и данные из INPUT / TEXTAREA, и прочее. ВСЕ ЭТО СТРОКИ!!!
+// ДИНАМИЧЕСКАЯ ТИПИЗАЦИЯ - ЭТО ВОЗМОЖНОСТЬ ОДНОГО ТИПА ДАННЫХ ПРЕВРАЩАТЬСЯ В ДРУГОЙ (строка в число, число в булиновое значение и т.д.)
+// СТАТИЧЕСЧКАЯ ТИПИЗАЦИЯ - ЭТО ЧИСЛО остается ЧИСЛОМ
+
+// МЕТОД to String - все что получаем от пользователя, всегда будет иметь тип STRING. Это и данные от PROMPT, и данные из INPUT / TEXTAREA, и прочее. ВСЕ ЭТО СТРОКИ!!!
 // 1)
-console.log(typeof(String(null))); // получил: string
-console.log(String(null)); // получил: null
-console.log(typeof(String(4))); // получил: string
+console.log(typeof(String(null))); // получил: string, так как метод String() переводит в строковый данный тип данных
+console.log(String(null)); // получил: null, так как метод String() переводит в строковый данный тип данных
+console.log(typeof(String(4))); // получил: string, так как метод String() переводит в строковый данный тип данных
 
 // 2)
-console.log(typeof(5 + '')); // получил:  string, ПРИ СЛОЖЕНИИ СО СТРОКОЙ ПОЛУЧАЕМ СТРОКУ!!!
+console.log(typeof(5 + '')); // получил:  string, ПРИ СЛОЖЕНИИ ЦИФР СО СТРОКОЙ ПОЛУЧАЕМ СТРОКУ!!!
 
 const num = 5;
-console.log('http://vk.com/catalog' + num); // получил: http://vk.com/catalog5, старый код, сейчас используется интерполяция, ссылка на каталог для динамического перехода на пятую строчку каталога!!!
+console.log('http://vk.com/catalog' + num); // получил: http://vk.com/catalog5, старый код, сейчас используется ИНТЕРПОЛЯЦИЯ, ссылка на каталог для динамического перехода на пятую строчку каталога!!!
 
 const fontSize = 26 + 'px'; // динамическая типизация при передаче стилей в строковом выражении
 console.log(fontSize); // получил: 26px
 
-// to Number
+// МЕТОД to Number
 // 1)
-console.log(typeof(Number('4'))); // получил: number, пользуются редко!
+console.log(typeof(Number('4'))); // получил: number, так как метод Number() переводит в числовой данный тип данных, но им пользуются редко! 
 
 // 2)
-console.log(typeof(+'5')); // получил: number, применяется унарный плюс чаще!
+console.log(typeof(+'5')); // получил: number, применяется унарный плюс, РАСТРОСТРАНЕННЫЙ МЕТОД! 
 
 // 3)
-console.log(parseInt('15px', 10)); // получил: 15, переводит в иное счисление строку
+console.log(parseInt('15px', 10)); // получил: 15, метод parseInt() переводит в иное счисление строку
 console.log(parseInt('15px' + 10)); // получил: 15,
 console.log(typeof(parseInt('15px', 10))); // получил: number
 
-// to Boolean
-// 1)
-// 0, '', null, undefined, NaN - превращается в FALSE!!!
-let switcher = null;
-if (switcher) {
-	console.log('Working...');  // получил: пусто, тут скрипт работать не будет так как в switcher значение FALSE!
+// МЕТОД to Boolean
+// 1) 0; '' - пустая строка; null - не существует; undefined - есть, но не определен; NaN - Not a Number => превращаются в FALSE!!!
+let switcher = null; // присвоим переменной switcher значение FALSE
+if (!switcher) { // если значение не FALSE, то работает =>
+	console.log('Working...');  // получил: Working...!!!
 }
-switcher = 1;
-if (switcher) {
-	console.log('Working...');  // получил: Working..., тут скрипт отработает так как в switcher значение TRUE!
+switcher = 1; // присвоим переменной switcher значение TRUE, так как это 1!!!
+if (switcher) { // если значение TRUE, то работает =>
+	console.log('Working...');  // получил: Working...!!!
 }
 
-// 2)
-console.log(typeof(Boolean('4')));  // получил: boolean, булиновое значение
+// 2) МЕТОД to Boolean
+console.log(typeof(Boolean('4')));  // получил: boolean, булиновое значение - логическое
 
-// 3)
+// 3)МЕТОД ДВУХ ОТРИЦАНИЙ
 console.log(typeof(!!'4444444'));  // получил: boolean, булиновое значение, крайне редко встречается!!!
