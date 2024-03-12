9cff8ab13e83f8e4f2242b02373187f0fab590c4
Denis <KILLDEN@RAMBLER.RU>
2024-03-12 16:10:04 +0300

    Update 12.03.24

--- 01_JS_Basics/24_lesson_async_defer_dynamic_scripts/js/script.js
+++ 01_JS_Basics/24_lesson_async_defer_dynamic_scripts/js/script.js
@@ -1,22 +1,22 @@
 // ПЕРВОЕ ПРИЛОЖЕНИЕ!!!
 
-// 'use strict';
+'use strict';
 
 const p = document.querySelectorAll('p');
 console.log(p);
 
-// const script = document.createElement('script');
-// script.src = 'js/test.js';
-// script.async = false;
-// document.body.append(script); // этот скрипт будет добавляться в html
+const script = document.createElement('script');
+script.src = 'js/test.js';
+script.async = false; // выполнение скриптов строго друг за другом
+document.body.append(script); // этот скрипт будет добавляться в html в конце body
 // после выполнения src="js/script.js", т.е. добавление будет с небольшой задержкой!
 // чтобы избежать этого, перед document.body.append(script); прописываем script.async = false;
 
 function loadScript(src) {
 	const script = document.createElement('script');
 	script.src = src;
-	script.async = false;
-	document.body.append(script);
+	script.async = false; // выполнение скриптов строго друг за другом
+	document.body.append(script); // этот скрипт будет добавляться в html в конце body
 }
 loadScript('js/test.js');
 loadScript('js/some.js'); // выполняются строго друг за другом!!!
