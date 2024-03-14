07e75f0f192632282cc1c8b4847e7630d408d30e
Denis <KILLDEN@RAMBLER.RU>
2024-03-14 18:40:20 +0300

    Update 14.03.24

--- 01_JS_Basics_add_2024/05_lesson_property_descriptors+useful_methods/js/script.js
+++ 01_JS_Basics_add_2024/05_lesson_property_descriptors+useful_methods/js/script.js
@@ -1,6 +1,10 @@
 'use strict';
 
-// 
+// ФЛАГИ:
+// WRITABLE - перезаписываемые, если флаг стоит в позиции true, то свойство объекта можно будет изменить, false - наоборот
+// ENUMERABLE - если флаг стоит в позиции true, то свойство объекта будет перечисляться в циклах, false - наоборот
+// CONFIGURABLE - если флаг стоит в позиции true, то свойство объекта можно будет удалить, а атрибуты можно изменить, false - наоборот
+
 const user = {
 	name: 'Alex',
 	surname: 'Smith',
@@ -9,3 +13,25 @@ const user = {
 		console.log(`${this.name} ${this.surname}`);
 	}
 };
+console.log(Object.getOwnPropertyDescriptor(user, 'name')); // метод Object.getOwnPropertyDescriptor() - первый аргумент: объект, второй аргумент: ключевое свойство, на котором буду применяться флаги!!!
+// получил:
+// {value: 'Alex', writable: true, enumerable: true, configurable: true}
+// configurable: true
+// enumerable: true
+// value: "Alex"
+// writable: true
+// [[Prototype]]: Object
+
+Object.defineProperty(user, 'name', {writable: false}); // метод Object.defineProperty() позволяет изменить флаги - первый аргумент: объект, второй аргумент: ключевое свойство, на котором буду применяться флаги, третий - сам флаг!!!
+// user.name = 'asdsdvbsfbds'; // получил: script.js:26  Uncaught TypeError: Cannot assign to read only property 'name' of object '#<Object>' at script.js:26:11
+
+// Создадим новое свойство
+Object.defineProperty(user, 'gender', {value: 'male'}); // метод Object.defineProperty() все ключевые флаги опционально выставил в false при ручном создании свойств объекта!!!
+console.log(Object.getOwnPropertyDescriptor(user, 'gender'));
+// получил: 
+// {value: 'male', writable: false, enumerable: false, configurable: false}
+// configurable: false
+// enumerable: false
+// value: "male"
+// writable: false
+// [[Prototype]]: Object
