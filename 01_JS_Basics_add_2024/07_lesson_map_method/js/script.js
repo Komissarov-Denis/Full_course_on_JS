31ea7a20e494de03cbedc6c99f3ee046bd1efc8d
Denis <KILLDEN@RAMBLER.RU>
2024-03-15 19:02:08 +0300

    Update 15.03.24

--- 01_JS_Basics_add_2024/07_lesson_map_method/js/script.js
+++ 01_JS_Basics_add_2024/07_lesson_map_method/js/script.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// 
+// MAP - картами называют спицефические виды данных, которые очень похожи на объект, только у них вместо свойств могут использоваться и объект, и массив, и функция, и все что угодно!!!
+
+// const user = {
+// 	name: 'Alex',
+// 	surname: 'Smith',
+// 	birthday: '20/04/1993',
+// 	showMyPublicData: function() {
+// 		console.log(`${this.name} ${this.surname}`);
+// 	}
+// };
+// console.log(typeof(Object.keys(user)[0])); // получил: name - string
+// console.log(typeof(Object.values(user)[0])); // получил: Alex - string
+
+const shops = [
+	{rice: 500},
+	{oil: 200},
+	{bread: 50},
+];
+const map = new Map();
+map.set(shops[0], 5000);
+console.log(map); // получил: Map(1) { { rice: 500 } => 5000 }
