ef8726c41fbd3592ce3e7097a42d5a8e6f7ca237
Denis <KILLDEN@RAMBLER.RU>
2024-03-15 17:37:10 +0300

    Update 15.03.24

--- 01_JS_Basics_add_2024/06_lesson_iterable_objects/js/script.js
+++ 01_JS_Basics_add_2024/06_lesson_iterable_objects/js/script.js
@@ -1,3 +1,63 @@
 'use strict';
 
 // ПЕРЕБИРАЕМЫЕ ОБЪЕКТЫ
+
+const user = {
+	name: 'Alex',
+	surname: 'Smith',
+	birthday: '20/04/1993',
+	showMyPublicData: function() {
+		console.log(`${this.name} ${this.surname}`);
+	}
+};
+
+// МЕТОД for (...in...) !!!ПЕРЕБИРАЕТ КЛЮЧЕВЫЕ ЗНАЧЕНИЯ!!! - метод для переборов массивов и даже строк, но он перебирает свойства не по порядку, а как получен массив (не рекомендован для широкого применения)!!!
+for (let key in user) {
+	console.log(user[key]);
+}
+// получил:
+// Alex
+// script.js:16 Smith
+// script.js:16 20/04/1993
+// script.js:16 ƒ () {
+// 		console.log(`${this.name} ${this.surname}`);
+// 	}
+
+const arr = ['b', 'a', 'c'];
+for (let key in arr) {
+	console.log(arr[key]);
+}
+// получил:
+// b
+// a
+// c
+
+const str = 'string';
+for (let key in str) {
+	console.log(str[key]);
+}
+// получил:
+// s
+// t
+// r
+// i
+// n
+// g
+
+
+// МЕТОД for (...of...) !!!ПЕРЕБИРАЕТ СВОЙСТВА КЛЮЧЕВЫХ ЗНАЧЕНИЙ!!! 
+const arr2 = ['b', 'a', 'c'];
+for (let key of arr2) {
+	console.log(key);
+}
+
+const arr3 = ['b', 'a', 'c'];
+console.dir(arr); // выводит сущность в виде объекта!!!
+Array.prototype.someMethod = function() {};
+for (let key of arr3) { // for (...of...) не выводит в список лишние сущности, как someMethod, в отличие от for (...in...)
+	console.log(key);
+}
+// получил:
+// b
+// a
+// c
