c809249e114669f17e205fbba5f5ac9f942efa48
Denis <KILLDEN@RAMBLER.RU>
2024-03-18 18:51:36 +0300

    Update 18.03.24

--- 01_JS_Basics_add_2024/08_lesson_set_data_type/js/script.js
+++ 01_JS_Basics_add_2024/08_lesson_set_data_type/js/script.js
@@ -24,32 +24,40 @@ console.log(setName); // получил: Set(4) {'Alex', 'Ann', 'Oleg', 'Ivan'}
 
 
 // // МЕТОД delete()
-// set.delete(value); // удаление значений
+set.delete(value); // удаление значений
+
 
 // // МЕТОД has()
-// set.has(value); // проверяем наличие значений
+set.has(value); // проверяем наличие значений
+
 
 // // МЕТОД clear()
-// set.clear(value); // полностью можем очистить set
+set.clear(value); // полностью можем очистить set
+
 
 // // МЕТОД size()
-// set.size(); // получение размера набора данных set
+set.size(); // получение размера набора данных set
+
 
 // МЕТОД перебора данных for (...of...)
 for (let value of set) console.log(value); // получил: 1, 2, 4, 5, 6
+
 
 // МЕТОД перебора данных forEach()
 setName.forEach((value, valueAgain) => {
 	console.log(value, valueAgain); // получил: Alex Alex   Ann Ann   Oleg Oleg   Ivan Ivan
 });
 
+
 // МЕТОД values() 
 setName.values();
 console.log(setName.values()); // получил: SetIterator {'Alex', 'Ann', 'Oleg', 'Ivan'}
 
+
 // МЕТОД keys()
 setName.keys();
 console.log(setName.keys()); // получил: SetIterator {'Alex', 'Ann', 'Oleg', 'Ivan'}
+
 
 // МЕТОД
 setName.entries();
