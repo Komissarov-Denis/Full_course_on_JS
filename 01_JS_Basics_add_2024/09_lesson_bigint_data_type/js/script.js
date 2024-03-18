ab41ee9bff581fee157858af816dca85a23a6195
Denis <KILLDEN@RAMBLER.RU>
2024-03-18 19:01:46 +0300

    Update 18.03.24

--- 01_JS_Basics_add_2024/09_lesson_bigint_data_type/js/script.js
+++ 01_JS_Basics_add_2024/09_lesson_bigint_data_type/js/script.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// 
+// console.log(Number.MAX_SAFE_INTEGER);
+// VM89:1 9007199254740991
+
+// BigInt - максимально доступное число для корректной работы!!!
+// BigInt нельзя использовать с методами со встроенным объектом Math{}!!!
+// BigInt нельзя смешивать в операциях с обычными числами!!!
+
+const bigint = 123218645494635168797435132198749n;
+
+const sameBigint = BigInt(123218645494635168797435132198749n);
+
+console.log(typeof(bigint));
+console.log(typeof(sameBigint));
