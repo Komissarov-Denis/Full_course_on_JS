fee15675685c8bf81f615de3e7e02bb78dc946fe
Denis <KILLDEN@RAMBLER.RU>
2024-04-01 18:39:03 +0300

    Update 01.04.24

--- 01_JS_Basics/32_lesson_Rest+default_parameter(ES6)/js/script.js
+++ 01_JS_Basics/32_lesson_Rest+default_parameter(ES6)/js/script.js
@@ -3,22 +3,25 @@
 'use strict';
 
 // Оператор REST записывается как(, ...rest) - применяется, когда мы не знаем какие еще аргументы будут применяться!!!
-const log = function(a, b, ...rest) {
+const log = function(a, b, ...rest) { // называть рест оператор можно как угодно, главное начинается он с многоточия ...с или ...fin
 	console.log(a, b, rest);
 };
 log('basic', 'else', 'operator', 'usage'); // получил - basic else [ 'operator', 'usage' ] - rest сущности оператор собрал в массив!!!
 
-//Параметры по умолчанию
-function calcOrDouble(number, basis) {
+
+// Параметры по умолчанию
+function calcOrDouble(number, basis) { // если аргумент basis не будет передан, то должен быть передан параметр по умолчанию
 	basis = basis || 2; // оператор ИЛИ вернет первую "правду", соответственно undefind || true и из этого выражения вернется 2, так как она имеет значение true
 	console.log(number * basis);
 }
 calcOrDouble(3); // но это не надежная проверка и в (ES6) параметры по умолчанию можно прописывать при объвлении функции =>
 
-function calcOrDouble2(number, basis = 2) {
+
+function calcOrDouble2(number, basis = 2) { // это вид записи (basis = 2) параметра по умолчанию
 	console.log(number * basis);
 }
 calcOrDouble2(3); // получил: 6
+
 
 function calcOrDouble3(number, basis) {
 	console.log(number * basis);
