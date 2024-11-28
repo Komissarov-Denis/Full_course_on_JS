8ff53537ec5df277703a6c11b1e8d0576f300d2d
Denis <KILLDEN@RAMBLER.RU>
2024-11-28 18:54:53 +0300

    Update 28.11.24

--- 05_React_Practice/Employees-App_project/my-app/src/components/employees-list-item/employees-list-item.js
+++ 05_React_Practice/Employees-App_project/my-app/src/components/employees-list-item/employees-list-item.js
@@ -1,31 +1,32 @@
-import { Component } from 'react';
+import { Component } from 'react'; // импортируем Component из React
 import './employees-list-item.css';
 // import { render } from '@testing-library/react';
 
-class EmployeesListItem extends Component { //  = ({name, salary, increase})
+class EmployeesListItem extends Component { //  = ({name, salary, increase}), class EmployeesListItem наследуем из Component React
+
 	constructor(props) {
 		super(props);
-		this.state = {
-			increase: false,
+		this.state = { // создаем состояние объекта state
+			increase: false, // передаем текущее значение false (не получает премию), это значение формирует предыдущее состояние, влияющее на будущее (если работник стал получать премию)
 		}
 	}
 
-	onIncrease = () => {
-		this.setState(({increase}) => ({
-			increase: !increase
+	onIncrease = () => { // формируем метод onIncrease, он зависит от предыдущего состояния (не получал премию), если стал получать, то необходимо применить коллбэк функцию
+		this.setState(({increase}) => ({ // это снова принцип деструктуризации с коллбэком
+			increase: !increase // данным способом просто меняем значение ключевого свойства объекта на обратное (false на true) при нажатии на печеньку/кнопку
 		}))
 	}
 
 	render() {  
-		const {name, salary} = this.props;
-		const {increase} = this.state;
+		const {name, salary} = this.props; // не зависят от предыдущего состояния класса EmployeesListItem
+		const {increase} = this.state; // зависят от предыдущего состояния класса EmployeesListItem
 
 		let classNames = "list-group-item d-flex justify-content-between";
 		if (increase) { // условие сравнивает наличие значения increase: true => 1
 			classNames += " increase"; // соответственно добавляет с пробелом класс "increase" в группу классов "list-group-item d-flex justify-content-between"
 		};
 
-		return (
+		return ( // на кнопку button назначаем обработчик onClick={this.onIncrease} с вызовом метода {this.onIncrease}
 			<li className={classNames}>
 				<span className="list-group-item-label">{name}</span>
 
