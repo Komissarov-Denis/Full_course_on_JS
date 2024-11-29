684ef8affe6a7087ca0b7e91c00f451756cd0c9e
Denis <KILLDEN@RAMBLER.RU>
2024-11-29 15:36:20 +0300

    Update 29.11.24

--- 05_React_Practice/Employees-App_project/my-app/src/components/employees-add-form/employees-add-form.js
+++ 05_React_Practice/Employees-App_project/my-app/src/components/employees-add-form/employees-add-form.js
@@ -2,30 +2,57 @@ import { Component } from 'react';
 
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-	return (
-		<div className="app-add-form">
-			<h3>Добавьте нового сотрудника</h3>
-			<form
-				className="add-form d-flex">
+class EmployeesAddForm extends Component{
 
-				<input type="text"
-					className="form-control new-post-label"
-					placeholder="Как его зовут?"
-				/>
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			salary: 0,
+		}
+	}
 
-				<input type="number"
-					className="form-control new-post-label"
-					placeholder="З/П в $?"
-				/>
+	onValueChangeName = (e) => { // создаем метод onValueChange с передачей ему в качестве аргумента события (е) изменения состояний name
+		this.setState({ // так как предыдущее состояние не влияет на текущее, применим обычный объект в setState()!!! (но можно и коллбэк применить)
+			name: e.target.value // e.target.value - это значение input, которое было передано в данное поле при вводе, передается в this.state{name: ''}
+		})
+	}
 
-				<button type="submit"
-					className="btn btn-outline-light">
-						Добавить
-				</button>
-			</form>
-		</div>
+	onValueChangeSalary = (e) => { // создаем метод onValueChange с передачей ему в качестве аргумента события (е) изменения состояний salary
+		this.setState({ // так как предыдущее состояние не влияет на текущее, применим обычный объект в setState()!!! (но можно и коллбэк применить)
+			salary: e.target.value // e.target.value - это значение input, которое было передано в данное поле при вводе, передается в this.state{salary: 0}
+		})
+	}
 
-	);
+	render () {
+		return (
+			<div className="app-add-form">
+				<h3>Добавьте нового сотрудника</h3>
+				<form
+					className="add-form d-flex">
+	
+					<input type="text"
+						className="form-control new-post-label"
+						placeholder="Как его зовут?"
+						name="name" // ключевое свойство name совпадает с this.state{name: ''} в конструкторе
+						onChange={this.onValueChangeName} // создаем событие с вызовом метода
+					/>
+	
+					<input type="number"
+						className="form-control new-post-label"
+						placeholder="З/П в $?"
+						salary="salary" // ключевое свойство salary совпадает с this.state{salary: 0} в конструкторе
+						onChange={this.onValueChangeSalary} // создаем событие с вызовом метода
+					/>
+	
+					<button type="submit"
+						className="btn btn-outline-light">
+							Добавить
+					</button>
+				</form>
+			</div>
+	
+		);
+	}
 }
 export default EmployeesAddForm;
