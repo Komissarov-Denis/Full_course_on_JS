6716db1f3432a10059efb9dfffcc5e5b05647f7c
Denis <KILLDEN@RAMBLER.RU>
2025-05-30 18:06:59 +0300

    Update 30.05.25

--- 05_React_Practice/Marvel-App_project/my-app/src/components/app/App.js
+++ 05_React_Practice/Marvel-App_project/my-app/src/components/app/App.js
@@ -1,3 +1,4 @@
+import { Component } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -5,20 +6,34 @@ import CharInfo from "../charInfo/CharInfo";
 
 import decoration from '../../resources/img/vision.png';
 
-const App = () => {
-    return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
-    )
+class App extends Component {
+
+	state = { // !!!для CharList и CharInfo применим прием ПОДЪЕМА СОСТОЯНИЯ, так как оба компонента находятся в одном родительском компоненте App и обмен данными пойдет через него!!!
+		selectedCharacter: null, // так как пока мы не выбрали ни одного персонажа, то выставим его состояние в "нуль"
+	}
+
+	onCharacterSelected = (id) => { // данный метод будет изменять состояние выбранного персонажа в приеме ПОДЪЕМА СОСТОЯНИЯ
+		this.setState({
+			selectedCharacter: id, // передаем идентификатор выбранному персонажу
+		})
+	} // и данный метод /onCharacterSelected()/ передадим в метод /render()/ в компонент /CharList/ как одноименную функцию по onCharacterSelected={this.onCharacterSelected} =>
+
+    render () { // в приеме ПОДЪЕМА СОСТОЯНИЯ, в компонент /CharInfo/ будем передавать ID персонажа по characterId={this.state.selectedCharacter} => т.е. из /CharList/ приходит /ID/, устанавливается в state и передается /CharInfo/
+	    return (
+			<div className="app">
+				<AppHeader/>
+				<main>
+					<RandomChar/>
+					<div className="char__content">
+						<CharList onCharacterSelected={this.onCharacterSelected}/> 
+						<CharInfo characterId={this.state.selectedCharacter}/>
+					</div>
+					<img className="bg-decoration" src={decoration} alt="vision"/>
+				</main>
+			</div>
+		)
+	}
+
 }
 
 export default App;
