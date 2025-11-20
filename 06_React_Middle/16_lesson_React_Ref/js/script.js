f9188634dfa12f07162c8446ca2b5b3282845fee
Denis <KILLDEN@RAMBLER.RU>
2025-11-20 15:35:15 +0300

    Update 20.11.25

--- 06_React_Middle/16_lesson_React_Ref/js/script.js
+++ 06_React_Middle/16_lesson_React_Ref/js/script.js
@@ -1,16 +1,15 @@
 // Стандартно родительский компонент может взаимодействовать с дочерним через props, мы можем через них передавать и данные, и методы, и состояние state.
 // Каждый раз, когда props изменяются, компонент перерисовывается, но возникает ситуация, когда нам не нужно перерисовывать компонент, поставить или снять фокус, 
 // узнать какой текст выделил пользователь и т.д. В таких случаях необходим прием с REF...
-// REF - это ссылка на элемент или компонент в DOM дереве в уже отрисованном интерфейсе на странице браузера, поэтому можно использовать DOM API и например метод focus() !!!
+// REF - это ссылка на элемент или компонент в DOM дереве и в уже отрисованном интерфейсе на странице браузера, поэтому можно использовать DOM API и например метод focus() !!!
 // REF могут отличаться в зависимости от того, на что повешен этот аттрибут: если на обычный элемент как input, то получим ссылку на этот элемент в DOM дереве, если повешен на
 // компонент, то свойство current будет получать экземпляр созданного компонента и это позволит вызывать методы самого этого компонента, на который повешен REF,
 // т.е. если бы на компоненте <Container/> был метод doSmth(), то мы бы его вызывали командой this.myRef.current.doSmth().
 // REF могут назначаться в ХУКАХ перед componentDidMount() и componentDidUpdate(), но когда компонент размонтируется, то в this.myRef = React.createRef() => в myRef = null,
 // т.е не нужно выполнять отписку, как это обычно бывает с обычными обработчиками событий или интервалами во избежание утечки памяти. 
-// REF нельзя назначать на функциональные компоненты так как они не не являются классами и они не создают экземпляры, соответственно мы компонент преобразуем в классовый и тогда
+// REF нельзя назначать на функциональные компоненты так как они не являются классами и они не создают экземпляры, соответственно мы компонент преобразуем в классовый и тогда
 // ошибки не будет!!!
 
-//--------------------------------------------------------BootstrapTest3.js
 import React, { Component } from "react";
 import { Container } from "react-bootstrap";
 
@@ -34,10 +33,10 @@ class Form extends Component {
 	//---------------------------------------------------
 
 	setInputRef = (elem) => { // второй способ создания REF, это коллбэк REF, это когда мы их создаем не при помощи createRef, а при помощи функции и записываем как экземпляр класса
-		this.myRef = elem; // setInputRef - это метод, принимающий в себя аргумент elem, мы создаем реф this.myRef  со ссылкой на elem
+		this.myRef = elem; // setInputRef - это метод, принимающий в себя аргумент elem, мы создаем реф this.myRef со ссылкой на elem
 	} // по факту у нас функция setInputRef создает новое поле this.myRef и в него помещает ссылку на этот elem
 
-	ficusFistTextInput = () => {
+	focusFistTextInput = () => {
 		if (this.myRef) { // проверяем наличие или создание элемента input, только тогда запускаем focus()
 			this.myRef.focus(); // в данном случае коллбэк рефа свойство current не требуется
 		}
@@ -55,16 +54,18 @@ class Form extends Component {
 							type="email" 
 							className="form-control" 
 							id="exampleFormControlInput1" 
-							placeholder="name@example.com"/>
+							placeholder="name@example.com"
+						/>
 					</div>
 					<div class="mb-3">
 						<label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-						<textarea onClick={this.ficusFistTI} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+						<textarea onClick={this.focusFistTextInput} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
 					</div>
 				</form>
             </Container>
-        ) // вызываем функцию setInputRef() посредством ref={this.setInputRef}, когда создастся DOM структура <input/>, запустится ref={this.setInputRef} и она возьмет elem, на котором она была вызвана и запишет его в this.myRef = elem, т.е. в ссылку внутри экземпляра класса
-    } // используем метод ficusFistTextInput() на элементе <textarea/> по средством обработчика onClick, тогда при фокусе на второй input - идет перенаправление на первый input
+        ) // вызываем функцию setInputRef() посредством ref={this.setInputRef}, когда создастся DOM структура <input/>, запустится ref={this.setInputRef} и она возьмет elem, на котором она была 
+		// вызвана и запишет его в this.myRef = elem, т.е. в ссылку внутри экземпляра класса
+    } // используем метод focusFistTextInput() на элементе <textarea/> по средством обработчика onClick, тогда при фокусе на второй input - идет перенаправление на первый input
 
 }
 export default Form;
